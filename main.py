6b93a023f04d9c4bb72f6cb73ed0ce7ff9c14794
Anthony Penaloza <apenaloza1@cpp.edu>
2025-11-10 11:10:59 -0800

    Adding functionality to states and transitions. Also fixed typos in code.

--- main.py
+++ main.py
@@ -22,28 +22,46 @@ class NFAmain:
         self.current_state = self.start_state 
 
 
-    def transition(self, ch);
+    def transition(self, ch):
 
         state = self.current_state
         
+            # Start State Logic
             if state == "START":
-                # TODO: handle '0' and '1-9' transitions, normal nfa/dfa start state
-                pass 
+                # Handles '0' and '1-9' transitions, normal nfa/dfa start state
+                if ch == '0':
+                    self.current_state = "ZERO"
+                elif ch.isdigit():
+                    self.current_state = "DIGIT"
+                else:
+                    self.current_state = "DEAD" 
 
+            # Zero State Logic
             elif state == "ZERO":
-                # TODO: stop accepting digits after '0'
-                pass
+                # Handles cases where digits/underscores follow zero, should be rejected
+                    self.current_state = "DEAD"
 
+            # Digit(s) State Logic
             elif state == "DIGIT":
-                # TODO: handle digits and underscores
-                pass
+                # Handles digits and underscores
+                if ch.isdigit():
+                    self.current_state = "DIGIT"
+                elif ch == "_":
+                    self.current_state = "UNDERSCORE"
+                else:
+                    self.current_state = "DEAD"
 
+            # Underscore State Logic
             elif state == "UNDERSCORE":
-                # TODO: acts as a space bar move onto next digit 
-                pass 
+                #  Handles cases of accept string if next ch is digit and reject if next ch is _ 
+                if ch.isdigit():
+                    self.current_state = "DIGIT"
+                else:
+                    self.current_state = "DEAD" 
 
+            # Dead State Logic
             else: 
-                # TODO: acts as a dead state, ends reading here
+                # Acts as a dead state, ends reading here
                 self.current_state = "DEAD"
 
     def run(self, s):
