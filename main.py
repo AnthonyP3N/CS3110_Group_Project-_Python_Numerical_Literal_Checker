489f8538eb11c01ac38a61a6c823e2e898b4c233
Anthony Penaloza <apenaloza1@cpp.edu>
2025-11-10 16:29:02 -0800

    Fixed indentation error in loops

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ class NFAmain:
 
         state = self.current_state
         
-            # Start State Logic
-            if state == "START":
+        # Start State Logic
+        if state == "START":
                 # Handles '0' and '1-9' transitions, normal nfa/dfa start state
                 if ch == '0':
                     self.current_state = "ZERO"
@@ -36,13 +36,13 @@ class NFAmain:
                 else:
                     self.current_state = "DEAD" 
 
-            # Zero State Logic
-            elif state == "ZERO":
+        # Zero State Logic
+        elif state == "ZERO":
                 # Handles cases where digits/underscores follow zero, should be rejected
                     self.current_state = "DEAD"
 
-            # Digit(s) State Logic
-            elif state == "DIGIT":
+        # Digit(s) State Logic
+        elif state == "DIGIT":
                 # Handles digits and underscores
                 if ch.isdigit():
                     self.current_state = "DIGIT"
@@ -51,29 +51,29 @@ class NFAmain:
                 else:
                     self.current_state = "DEAD"
 
-            # Underscore State Logic
-            elif state == "UNDERSCORE":
+        # Underscore State Logic
+        elif state == "UNDERSCORE":
                 #  Handles cases of accept string if next ch is digit and reject if next ch is _ 
                 if ch.isdigit():
                     self.current_state = "DIGIT"
                 else:
                     self.current_state = "DEAD" 
 
-            # Dead State Logic
-            else: 
-                # Acts as a dead state, ends reading here
-                self.current_state = "DEAD"
+        # Dead State Logic
+        else: 
+            # Acts as a dead state, ends reading here
+            self.current_state = "DEAD"
 
     def run(self, s):
         self.reset()
 
-            for ch in s:
+        for ch in s:
                 self.transition(ch)
                 if self.current_state == "DEAD":
                     break 
 
-            # Accept string if it ends in one of the valid accept states
-            return self.current_state in self.accept_states 
+        # Accept string if it ends in one of the valid accept states
+        return self.current_state in self.accept_states 
 
 
 
